f5aee660b9f1c080b34de1814c32a14677b1c74f
Masahiro TANAKA <masa16.tanaka@gmail.com>
2013-02-27 22:15:02 +0900

    revert checking in extconf.rb

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -11,10 +11,15 @@
 
 # Narray is now Gem based, so require rubygems
 # so that we can use Gem class to find narray.
-require 'rubygems'
 require "mkmf"
 
 #$DEBUG = true
+
+# configure options:
+#  --with-x11-dir=path
+#  --with-x11-include=path
+#  --with-x11-lib=path
+dir_config("x11")
 
 # configure options:
 #  --with-pgplot-dir=path
@@ -35,13 +40,23 @@ def find_dir_w_file(d,h)
   File.dirname(g.last) if g and !g.empty?
 end
 
+def find_dir_in_gemspec
+  begin
+    require 'rubygems'
+    if gemspec=Gem::Specification.find_by_path('narray')
+      return File.join(gemspec.full_gem_path, gemspec.require_path)
+    end
+  rescue
+  end
+  nil
+end
+
 # Check NArray
-na_gemspec=Gem::Specification.find_by_path('narray')
-narray_h_dir=File.join(na_gemspec.full_gem_path, na_gemspec.require_path) if na_gemspec
 gems_dir="$(rubylibprefix)/gems/$(ruby_version)/gems/"
 narray_d="narray-0.[56].*"
 narray_h="narray.h"
-if narray_h_dir ||=
+if narray_h_dir =
+    find_dir_in_gemspec ||
     find_dir_w_file("../"+narray_d,narray_h) ||
     find_dir_w_file(gems_dir+narray_d,narray_h) ||
     find_dir_w_file(CONFIG['sitearchdir'],narray_h) ||
@@ -55,6 +70,29 @@ if RUBY_PLATFORM =~ /cygwin|mingw/
   exit unless have_library("narray","na_make_object")
 end
 
+# Check FORTRAN Libraries
+#
+# SUN WorkShop FORTRAN 77 compiler ver5.0
+# configure options: --with-sunws
+if with_config("sunws")
+  $libs = "-lM77 -lsunmath "+$libs
+  exit unless find_library("F77", "f77_init", "/opt/SUNWspro/lib")
+  $defs.push "-DSPARC_FORTRAN"
+#
+# GNU FORTRAN v4
+elsif have_library("gfortran")
+  $CFLAGS = "-Wall "+$CFLAGS
+  $defs.push "-DGNU_FORTRAN"
+#
+# GNU FORTRAN v3
+elsif have_library("g77")
+  $CFLAGS = "-Wall "+$CFLAGS
+  $defs.push "-DGNU_FORTRAN"
+else
+  puts "failed"
+  exit
+end
+
 # Check GrWin Library (for cygwin (and mingw32?))
 #  configure options: --with-grwin
 if with_config("grwin")
@@ -63,6 +101,16 @@ if with_config("grwin")
     $libs += " -mwindows"
   end
   exit unless have_library("GrWin", "GWinit")
+end
+#
+# Check X11 Library
+have_library("X11", "XOpenDisplay")
+
+# Check PNG Library
+libs_save = $libs
+$libs = append_library($libs, "z")
+if !have_library("png","png_create_write_struct")
+  $libs = libs_save
 end
 
 # Check PGPLOT Library
