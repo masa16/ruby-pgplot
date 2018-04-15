b91472a55b38414c80852b28478df55b8f4c188f
Masahiro TANAKA <masa16.tanaka@gmail.com>
2018-04-15 19:07:28 +0900

    patch to pndriv.c

---
+++ ext/build_lib/patch.rb
@@ -0,0 +1,14 @@
+require 'fileutils'
+
+fnw = File.join($build_dir,"drivers/pndriv.c")
+fnr = fnw + ".orig"
+
+FileUtils.mv(fnw,fnr)
+
+open(fnr,"rt") do |r|
+  open(fnw,"wt") do |w|
+    while s = r.gets
+      w.print s.sub(/png_ptr->jmpbuf/,"png_jmpbuf(png_ptr)")
+    end
+  end
+end
